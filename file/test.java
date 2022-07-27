962e0e013f7be46b0a2a910602b2faee7e45e70c
DecaprioLee <1805182585@qq.com>
2022-07-27 21:36:14 +0800

    secondadd

---
+++ file/test.java
@@ -0,0 +1,5 @@
+public class test{
+    static int name;
+    double f=2.12;
+    System.out.print("name"+f);
+}
--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
     void public static void main(String[] args) {
         System.out.print("hello java");
+        
     }
 }
