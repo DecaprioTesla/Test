810094796caef93d3d8b198d8f15766b797cb805
DecaprioLee <1805182585@qq.com>
2022-09-18 14:30:44 +0800

    dev 修改

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 public class hello{
     void public static void main(String[] args) {
         System.out.print("hello java");
-        System.out.print("first commit");;
+        System.out.print("first commit");
+        System.out.println("dev submit");
         
     }
 }
